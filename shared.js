dc4ddf1efbf89d3b57a969115cb0955067c00e61
Hiteshyadav163 <hiteshyadav1603@gmail.com>
2026-04-19 17:25:58 +0530

    updated number and email

--- shared.js
+++ shared.js
@@ -176,3 +176,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
   document.querySelectorAll('.reveal').forEach(el => revealObs.observe(el));
 });
+
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  reveals.forEach((el) => {
+    const top = el.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (top < windowHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
